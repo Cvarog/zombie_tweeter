73720c0a88a9e929256758a6c1cd5b8a9f2aaa99
Artem Shaykin <uchihamilord@gmail.com>
2012-12-06 15:24:31 +0400

    Update app/controllers/tweets_controller.rb
    Comments works!!!

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,18 +4,16 @@ class TweetsController < ApplicationController
 
 	def create
 	@zombie = Zombie.find(params[:zombie_id])
-  	@tweet = @zombie.tweets.build(:status => params[:tweet], :zombie_id => params[:zombie_id])
-  	redirect_to @tweet.zombie, :notice => params
-
+  	@tweet = @zombie.tweets.create(params[:tweet])
+  	# redirect_to @tweet.zombie
+  	redirect_to zombie_path(@zombie)
   	end
 
 	def destroy
+	@zombie = Zombie.find(params[:zombie_id])
 	@tweet = Tweet.find(params[:id]) 
   	@tweet.destroy
-
-  	redirect_to @tweet.zombie
-
-
+  	redirect_to zombie_path(@zombie)
 	end
 
 end
