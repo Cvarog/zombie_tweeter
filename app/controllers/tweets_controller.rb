2528f6278021904b6947a427448190cb61305150
Cvarog <uchihamilord@gmail.com>
2012-12-10 02:22:06 +0400

    Haml exp

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,16 +4,17 @@ class TweetsController < ApplicationController
 
 	def create
 	@zombie = Zombie.find(params[:zombie_id])
-  	@tweet = @zombie.tweets.build(:status => params[:tweet], :zombie_id => params[:zombie_id])
-  	redirect_to @tweet.zombie, :notice => params
+  @tweet = @zombie.tweets.create(params[:tweet])
+  	# redirect_to @tweet.zombie
+  redirect_to zombie_path(@zombie)#, :notice => params[:tweet]
 
   	end
 
 	def destroy
 	@tweet = Tweet.find(params[:id]) 
-  	@tweet.destroy
+  @tweet.destroy
 
-  	redirect_to @tweet.zombie
+  redirect_to @tweet.zombie
 
 
 	end
